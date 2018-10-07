0bf0a8c69272061aa3c29a5c7533c37c2e916947
AmitThakare <amit.thakare28@gmail.com>
2018-10-07 16:49:23 +0530

    Update CrossBrowserTest.java

--- MavenProject/src/test/java/com/QA/CrossBrowserTest.java
+++ MavenProject/src/test/java/com/QA/CrossBrowserTest.java
@@ -12,7 +12,7 @@ public class CrossBrowserTest {
 
 	public WebDriver driver;
 	
-	@BeforeClass//add comment
+	@BeforeClass
 	@Parameters("Browser")
 		public void ParallelTestExecute(String Browser)
 	{
